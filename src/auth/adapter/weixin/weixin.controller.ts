65adc63db15817eac7d265a735693cd8feb58b12
380989353 <380989353@qq.com>
2020-11-24 23:29:40 +0800

    接通获取微信access-token接口

--- src/auth/adapter/weixin/weixin.controller.ts
+++ src/auth/adapter/weixin/weixin.controller.ts
@@ -6,7 +6,12 @@ export class WeixinController {
     constructor(private readonly weixinService: WeixinService){}
 
     @Get('getOpenid')
-    getOpenid(@Query('appid') appid:string, @Query('secret') secret: string){
-        return this.weixinService.getOpenid(appid,secret)
+    getOpenid(@Query('appid') appid:string, @Query('secret') secret: string, @Query('code') code: string){
+        return this.weixinService.getOpenid(appid,secret,code)
+    }
+
+    @Get('getAccessToken')
+    getAccessToken(@Query('appid') appid:string, @Query('secret') secret: string){
+        return this.weixinService.getAccessToken(appid,secret)
     }
 }
--- src/auth/adapter/weixin/weixin.service.ts
+++ src/auth/adapter/weixin/weixin.service.ts
@@ -4,7 +4,13 @@ import { HttpService, Injectable } from '@nestjs/common';
 export class WeixinService {
     constructor(private readonly httpService: HttpService){}
 
-    getOpenid(appid:string, secret:string){
-        return this.httpService.get(`https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${appid}&secret=${secret}`)
+    getOpenid(appid:string, secret:string, code: string){
+        console.log('123',appid,secret)
+        return this.httpService.get(`https://api.weixin.qq.com/sns/jscode2session?grant_type=authorization_code&appid=${appid}&secret=${secret}&js_code=${code}`)
+    }
+    
+    async getAccessToken(appid:string, secret:string){
+        const res = await this.httpService.get(`https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${appid}&secret=${secret}`).toPromise()
+        return res.data
     }
 }
