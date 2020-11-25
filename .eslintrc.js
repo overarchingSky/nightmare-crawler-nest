b8930f090347e5f4d89f613d1990bbf9d36ef934
tl <tianlong@streamax.com>
2020-11-25 14:44:48 +0800

    添加项目代码格式化配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
     jest: true,
   },
   rules: {
+    'indent':4,
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
