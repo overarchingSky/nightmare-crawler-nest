166fa67e75e725435f5c5baa56d8ce9e3634df03
380989353@qq.com <380989353@qq.com>
2020-12-06 15:36:09 +0800

    补充个人信息字段

--- src/personal-information/dto/personalInfomation.dto.ts
+++ src/personal-information/dto/personalInfomation.dto.ts
@@ -1,6 +1,110 @@
+export interface IAddress {
+    province:number
+    city:number
+    area:number
+}
+
+export enum EnumMaritalStatus{
+    /**
+     * 未婚
+     * 
+     */
+    UNMARRIED,
+    /**
+     * 短婚未育
+     */
+    MARRIAGEDNOBIRTH,
+    /**
+     * 有男孩
+     */
+    MARRIAGEDWITHBOY,
+    /**
+     * 有女孩
+     */
+    MARRIAGEDWITHGIRL,
+    /**
+     * 丧偶
+     */
+    WIDOWED
+}
+
+export enum Enumeducation{
+    /**
+     * 高中
+     */
+    HIGHSCHOOLDEGREE,
+    /**
+     * 大专
+     */
+    COLLEGEDEGREE,
+    /**
+     * 本科
+     */
+    BACHELORDEGREE,
+    /**
+     * 硕士
+     */
+    MASTERDEGREE,
+    /**
+     * 博士
+     */
+    DOCTORDEGREE
+}
+
+export enum EnumSource{
+    /**
+     * 朋友介绍
+     */
+    FRIEND,
+    /**
+     * 知乎
+     */
+    ZHIHU,
+    /**
+     * 豆瓣
+     */
+    DOUBAN,
+    /**
+     * 小红书
+     */
+    XIAOHONGSHU,
+    /**
+     * 抖音
+     */
+    DOUYIN,
+    /**
+     * 今日头条
+     */
+    JINRITOUTIAO,
+    /**
+     * 微视频
+     */
+    WEISHIPIN
+}
+
 export interface IPersonalInfomation {
     accountId?:string
     name:string
-    age:number
     gender:'MALE' | 'FEMAL'
+    phone:number
+    birthday:number
+    height:number
+    weight:number
+    member:boolean
+    idcard:string
+    permanentAddress:IAddress
+    occupation:string
+    workAddress:IAddress
+    maritalStatus:EnumMaritalStatus
+    familyMember:string
+    education:Enumeducation
+    educationCertificateNumber:string
+    school:string
+    hasCar:boolean
+    income:number
+    hasHouseProperty:boolean
+    source:EnumSource
+    introduction:string
+    expect:string
+    images:string[]
 }
--- src/personal-information/schemas/personal-infomation.schemas.ts
+++ src/personal-information/schemas/personal-infomation.schemas.ts
@@ -1,6 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
-import { IPersonalInfomation } from '../dto/personalInfomation.dto';
+import { IPersonalInfomation, IAddress, EnumMaritalStatus, Enumeducation, EnumSource } from '../dto/personalInfomation.dto';
 
 @Schema()
 export class PersonalInfomation extends Document implements IPersonalInfomation {
@@ -10,11 +10,119 @@ export class PersonalInfomation extends Document implements IPersonalInfomation 
   @Prop({ required: true })
   name: string;
 
-  @Prop({ required: true })
-  age: number;
-
   @Prop()
   gender: 'MALE' | 'FEMAL';
+
+  @Prop({ required: true })
+  phone: number;
+
+  @Prop({ required: true })
+  birthday: number;
+
+  @Prop({ required: true })
+  height: number;
+
+  @Prop({ required: true })
+  weight: number;
+
+  /**
+   * 是否是会员
+   */
+  @Prop({ required: true })
+  member: boolean;
+
+  @Prop({ required: true })
+  idcard: string;
+
+  /**
+   * 户籍地址
+   */
+  @Prop()
+  permanentAddress: IAddress;
+
+  /**
+   * 职业
+   */
+  @Prop({ required: true })
+  occupation: string;
+
+  /**
+   * 工作区域
+   */
+  @Prop({ required: true })
+  workAddress: IAddress;
+
+  /**
+   * 婚姻状况
+   */
+  @Prop({ required: true })
+  maritalStatus: EnumMaritalStatus;
+
+  /**
+   * 家庭成员
+   */
+  @Prop()
+  familyMember: string;
+  
+  /**
+   * 学历
+   */
+  @Prop({ required: true })
+  education: Enumeducation;
+  
+  /**
+   * 学历证号
+   */
+  @Prop()
+  educationCertificateNumber:string
+
+  /**
+   * 毕业学校
+   */
+  @Prop()
+  school:string
+
+  /**
+   * 是否拥有汽车
+   */
+  @Prop({ required: true })
+  hasCar:boolean
+
+  /**
+   * 年收入
+   */
+  @Prop({ required: true })
+  income:number
+  
+  /**
+   * 有无房产
+   */
+  @Prop({ required: true })
+  hasHouseProperty:boolean
+
+  /**
+   * 客户来源、渠道
+   */
+  @Prop({ required: true })
+  source:EnumSource
+
+  /**
+   * 个人简介
+   */
+  @Prop()
+  introduction:string
+
+  /**
+   * 对另一半的期望
+   */
+  @Prop()
+  expect:string
+
+  /**
+   * 本人照片
+   */
+  @Prop({ required: true })
+  images:string[]
 }
 
 export const PersonalInfomationSchema = SchemaFactory.createForClass(
